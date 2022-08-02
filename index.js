8f278c490e9a0889321057582337ebe1684b9f95
Nijyar Ghazi <nijyar360@gmail.com>
2022-08-03 00:45:02 +0300

    Completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+let main = document.querySelector("main#main");
+main.remove();
+
+let body = document.querySelector("body");
+
+let newHeader = document.createElement("h1");
+
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML= "Nijyar is the champion"
+
+body.append(newHeader);
+
